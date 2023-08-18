10d580c0f3b28a09953078a29f611b993f3d53c4
jwywoo <jwywoo26@gmail.com>
2023-08-18 13:27:38 +0900

    First Commit: Project setting

---
+++ src/EvenNms/TesTwo.java
@@ -0,0 +1,11 @@
+package EvenNms;
+public class TesTwo {
+    static double solution(int[] arr) {
+        double answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
