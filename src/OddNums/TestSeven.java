ed9ca974037fe468f84820cdc1a9e135d77a1ad7
jwywoo <jwywoo26@gmail.com>
2023-08-18 16:33:18 +0900

    Coding Test Odd Num 1-10 done

---
+++ src/OddNums/TestSeven.java
@@ -0,0 +1,18 @@
+package OddNums;
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
+// 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+// 1. n을 String으로 변환한다
+// 2. Stack을 만든다.
+// 3. 순회를 이용해 만들어논 Stack에 string이된 N값을 넣는다.
+// 4. answer의 길이른 string으로 바꾼 n의 길이와 일치 시킨다
+// 4. 순회를 한번더 해서 Stack에 있는 값을 answer에 담는다.
+public class TestSeven {
+    static int[] solution(long n) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
