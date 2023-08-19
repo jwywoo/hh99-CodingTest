a9240fc3788cb918532214fc45c2eb76c52c1dc0
jwywoo <jwywoo26@gmail.com>
2023-08-19 15:55:32 +0900

    Jwywoo: 10-20 Even nums done

---
+++ src/EvenNms/Test14.java
@@ -0,0 +1,20 @@
+package EvenNms;
+//String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+//
+//제한 사항
+//seoul은 길이 1 이상, 1000 이하인 배열입니다.
+//seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+//"Kim"은 반드시 seoul 안에 포함되어 있습니다.
+
+// 1. for 문을 돌면서 equals를 이용해 Kim의 Index를 찾는다.
+// 2. answer에 직접 "김서방은 " + (index)+ "에 있다" 추가한다.
+public class Test14 {
+    static String solution(String[] seoul) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
